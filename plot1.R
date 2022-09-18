c2111b8a8f375ffeed503325643a8ab7f6bf970d
ShaikHaniya <111421861+ShaikHaniya@users.noreply.github.com>
2022-09-18 14:39:34 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,11 @@
 Filepath <- "E:\household_power_consumption.txt"
 data <- read.table(Filepath, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+#creating  a subset of data with dates we need
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 globalActivePower <- as.numeric(subSetData$Global_active_power)
+#plotting the png with the mentioned dimensions
 png("plot1.png", width=480, height=480)
+#plotting a histogram
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+#showing the plot
 dev.off()
