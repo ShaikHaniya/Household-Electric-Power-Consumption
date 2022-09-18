7dede11e64787a23119310522e35b06a0928a784
ShaikHaniya <111421861+ShaikHaniya@users.noreply.github.com>
2022-09-18 14:44:24 +0530

    code

--- plot1.R
+++ plot1.R
@@ -7,5 +7,5 @@ globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot1.png", width=480, height=480)
 #plotting a histogram
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-#showing the plot
+#showing the plot 
 dev.off()
